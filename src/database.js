a316ea61edbcd183819e95b9fc63daeb0d996767
Juanjoc12 <36391236+Juanjoc12@users.noreply.github.com>
2020-10-20 17:52:17 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://admin:Slipknot12@cluster0.eq4qd.mongodb.net/RetoBD?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://admin:Contraseña@cluster0.eq4qd.mongodb.net/NombreBD?retryWrites=true&w=majority';
 
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db => console.log('DB is connected'))
